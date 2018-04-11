c625e68871ff33ca14a76784e09d2e245c10b3e3
Aaron Kling <webgeek1234@gmail.com>
2018-04-11 11:45:31 -0500

    Refactor android jni makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,38 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+ROOT_DIR     := $(LOCAL_PATH)/..
+CORE_DIR     := $(ROOT_DIR)/source
+LIBRETRO_DIR := $(ROOT_DIR)
+
+LOAD_FROM_MEMORY_TEST := 1
+FLAGS :=
+
+include $(ROOT_DIR)/Makefile.common
+
+COREFLAGS := -ffast-math $(FLAGS)
 
 GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
 ifneq ($(GIT_VERSION)," unknown")
-	LOCAL_CFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+  COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
 endif
 
-CORE_DIR     := ../source
-LIBRETRO_DIR := ..
-
-DEBUG                 = 0
-PERF_TEST             = 0
-LOAD_FROM_MEMORY_TEST = 1
-USE_BLARGG_APU        = 0
-
+include $(CLEAR_VARS)
 LOCAL_MODULE    := retro
-
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_CFLAGS += -DANDROID_ARM
-LOCAL_ARM_MODE := arm
-endif
-
-ifeq ($(TARGET_ARCH),x86)
-LOCAL_CFLAGS +=  -DANDROID_X86
-endif
-
-ifeq ($(TARGET_ARCH),mips)
-LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
-endif
-
-include ../Makefile.common
-
-LOCAL_SRC_FILES += $(SOURCES_C)
-LOCAL_CFLAGS    += -std=gnu99 -ffast-math $(FLAGS)
-
+LOCAL_SRC_FILES := $(SOURCES_C)
+LOCAL_CFLAGS    := $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(LIBRETRO_DIR)/link.T
 include $(BUILD_SHARED_LIBRARY)
